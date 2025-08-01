02e347f4aba96feb4d3afbf9ae6c67f21785d94a
JishNet <jishan6263487341@gmail.com>
2025-08-01 18:20:04 +0530

    reverse a string with the help of Recursion

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 class Main {
-    static String reverse_String(String st) {
+    static String reverse_String(String st,int idx) {
+        if(idx>=st.length()) return "" ;
         String ans = new String () ;
-        for(int i = st.length()-1 ;i>=0 ;i--){
-               ans += st.charAt(i) ;
 
-        }
+         String s =  reverse_String(st,idx+1) ;
+         ans += st.charAt(idx) ;
 
-        return  ans  ;
+
+
+        return  s+ans  ;
     }
     public static void main(String... args) {
         String st = "Jishan" ;
 
-        System.out.println(reverse_String(st)) ;
+        System.out.println(reverse_String(st,0)) ;
 
 
     }
